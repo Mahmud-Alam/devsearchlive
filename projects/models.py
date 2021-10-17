b119727f76c87d8194359ad008b76fef970a8093
Mahmud-Alam <official.mahmud23@gmail.com>
2021-10-17 10:40:58 +0600

    Database Relationships

--- projects/models.py
+++ projects/models.py
@@ -1,15 +1,50 @@
 from django.db import models
 import uuid
 
+from django.db.models.base import Model
+
 class Project(models.Model):
+    #owner = 
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     title = models.CharField(max_length = 200)
     description = models.TextField(null = True, blank=True)
+    #featured_image = 
     demo_link = models.CharField(max_length=1000, null = True, blank=True)
     source_link = models.CharField(max_length=1000, null = True, blank=True)
     vote_total = models.IntegerField(default=0)
     vote_ratio = models.IntegerField(default=0)
-    created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    created = models.DateTimeField(auto_now_add=True) #save time once when created
+    updated = models.DateTimeField(auto_now=True) #save time every time when updated
     
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    
+    VOTE_TYPE = (
+        ('up','up'),
+        ('down','down')
+    )
+
+    #owner = 
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True,blank=True)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=50,choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
