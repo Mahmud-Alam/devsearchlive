df7ab196ac1e17c7ce75a4c9c09d68c3a8e754b8
Mahmud-Alam <official.mahmud23@gmail.com>
2021-10-18 13:56:26 +0600

    show images for perticular single-project page

--- projects/models.py
+++ projects/models.py
@@ -8,7 +8,7 @@ class Project(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     title = models.CharField(max_length = 200)
     description = models.TextField(null = True, blank=True)
-    #featured_image = 
+    featured_image = models.ImageField(null=True, blank=True)
     demo_link = models.CharField(max_length=1000, null = True, blank=True)
     source_link = models.CharField(max_length=1000, null = True, blank=True)
     vote_total = models.IntegerField(default=0)
@@ -19,6 +19,14 @@ class Project(models.Model):
     
     def __str__(self):
         return self.title
+
+    @property
+    def imageURL(self):
+        try:
+            img = self.featured_image.url
+        except:
+            img = ''
+        return img
 
 
 class Review(models.Model):
