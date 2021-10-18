2bcec271c51193e1b2a55d7dbfa18e19f120a847
Mahmud-Alam <official.mahmud23@gmail.com>
2021-10-18 16:35:30 +0600

    after setting  DEBUG=False try to fix site issues and installed WhiteNoise for staticfiles

--- devsearchlive/settings.py
+++ devsearchlive/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,6 +133,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
