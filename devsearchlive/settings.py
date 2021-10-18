0c7e48a436278c35e4d949018f0f95ac3325d6e5
Mahmud-Alam <official.mahmud23@gmail.com>
2021-10-18 14:37:07 +0600

    set DEBUG=False

--- devsearchlive/settings.py
+++ devsearchlive/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3trh0@j8hqet*y2sb=5$^@ahrjv91ub($8@%()u!d=7u$l#i3('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
